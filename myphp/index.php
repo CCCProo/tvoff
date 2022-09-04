e671769ddacb73ee3a7f3250e5af7490243e9301
Kirill Shishkov <93086215+CCCProo@users.noreply.github.com>
2022-09-04 13:48:09 +0300

    Add files via upload

---
+++ myphp/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= "Новяя страница"; ?></title>
+</head>
+<body>
+	<?php
+	   echo date(DATE_RSS);
+	   ?>
+</body>
+</html>
